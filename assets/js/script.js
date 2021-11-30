7a6f83601f6050a994ed2a344daf0d1fdba634b9
Nicholas Nolen <nicnolen@ymail.com>
2021-11-30 12:47:14 -0600

    added the ability to edit task dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,9 +74,9 @@ var text = $(this)
 
 // get the parent ul's id attribute
 var status = $(this)
-  .closest(".list group")
+  .closest(".list-group")
   .attr("id")
-  .replace("list-", "");
+  .replace("list-", "")
 
 // get the task's position in the list of other li elements
 var index = $(this)
@@ -94,6 +94,57 @@ var taskP = $("<p>")
 
 // replace textarea with p element
 $(this).replaceWith(taskP);
+});
+
+// Add event listener for child element (`span`) to parent `ul` element when the due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+  .text()
+  .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// Convert inputs back to paragraphs when the user clicks outside (when the element's blur event occurs)
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+    // get the parent ul's id attribute
+    var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "")
+      
+    // get the task's position in the list of other li elements
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
+
+    // update task in array and re-save to localStorage
+    tasks[status][index].date = date; 
+    saveTasks();
+
+    // recreate span element with bootstrap classes 
+    var taskSpan = $("<span>")
+      .addClass("badge badge-primary badge-pill")
+      .text(date);
+
+    // replace input with span element
+    $(this).replaceWith(taskSpan);
 });
 
 // modal was triggered
