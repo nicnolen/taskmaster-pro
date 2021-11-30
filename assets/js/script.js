e62a48fabe36bdefb2a6eeeb65326ef73a817e06
Nicholas Nolen <nicnolen@ymail.com>
2021-11-30 11:34:04 -0600

    created task editing for the task description

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
-  var taskLi = $("<li>").addClass("list-group-item");
+  var taskLi = $("<li>").addClass("list-group-item"); // The `$` symbols represent jQuery references. 
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -41,12 +40,61 @@ var loadTasks = function() {
   });
 };
 
+// Save tasks to local storage
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// Attach click event for the child elements (`p`) to the parent (ul with class `.list-group`)
+$(".list-group").on("click", "p", function() {
+  // get the inner text content of the current element `$(this)
+  var text = $(this)
+  // remove whitespace before or after incase user accidentally puts in extra space
+  .text()
+  .trim();
 
+  // Create a new `textArea` element and save it in the textInput variable
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  
+  // swap out existing `p` element with the new `textArea` element
+  $(this).replaceWith(textInput);
 
+  // highlight (focus) the input box
+  textInput.trigger("focus");
+  });
+
+// Make `textArea` revert back to `p` when it goes out of focus
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+// update the tasks object with new data
+tasks[status][index].text = text;
+saveTasks();
+
+// recreate p elements
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
